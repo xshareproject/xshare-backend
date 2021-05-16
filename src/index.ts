1c3dcb2c25de4f6b55bc24595d8d235925742187
Kendric Chung <kendiv3@gmail.com>
2021-05-16 13:41:12 -0700

    abstract mongoose connection to function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import * as http from "http";
@@ -8,34 +7,20 @@ import debug from "debug";
 import * as winston from "winston";
 import * as expressWinston from "express-winston";
 
+import { connectToMongoDatabase } from "./common/mongoose/mongoose.service";
+
 import { CommonRoutesConfig } from "./common/common.routes.config";
 import { UserRoutes } from "./api/user/user.routes.config";
 
 dotenv.config();
+
+connectToMongoDatabase();
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 const routes: CommonRoutesConfig[] = [];
 const debugLog: debug.IDebugger = debug("app");
-
-const MONGO_USER = process.env.MONGO_USER;
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_DB = process.env.MONGO_DB;
-
-const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.wcu0n.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`;
-
-mongoose.connect(
-  uri,
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-  (err: any) => {
-    if (err) {
-      console.log("Error, not connected");
-    } else {
-      console.log("Connected as: ", MONGO_USER, " with DB: ", MONGO_DB);
-    }
-  }
-);
 
 app.use(express.json());
 
